fd22fb63824cbb4ebd5f1a73332816d5f290674e
sun-eng <1799965181@qq.com>
2020-03-25 17:44:25 +0800

    公司框架

--- demo-boot/src/main/java/com/nhsoft/demo/boot/DemoBootApplication.java
+++ demo-boot/src/main/java/com/nhsoft/demo/boot/DemoBootApplication.java
@@ -2,8 +2,10 @@ package com.nhsoft.demo.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:config.xml")
 public class DemoBootApplication {
 
     public static void main(String[] args) {
